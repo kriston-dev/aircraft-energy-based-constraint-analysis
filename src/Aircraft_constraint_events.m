b54a415b08d0b1080a9f6a815e1cf8ee7e8caa0d
Kriston Rickman <kristonrickman@gmail.com>
2026-09-03 09:20:37 -0700

    v1_00
    States and Events for main program

---
+++ src/Aircraft_constraint_events.m
@@ -0,0 +1,18 @@
+%Created by Kriston Rickman
+%Date created 09/03/26
+%v1_00 for Energy based constraint analysis v1_19
+%Events
+%Notes: 
+
+
+
+classdef Aircraft_constraint_events
+    enumeration
+        SELECT_FIND_TW
+        AIRCRAFT_DATA_ENTERED
+        PERFORMANCE_DATA_ENTERED
+        CALCULATION_COMPLETE
+        INVALID_INPUT
+        RETURN_TO_MENU
+    end
+end
---
+++ src/Aircraft_constraint_states.m
@@ -0,0 +1,19 @@
+%Created by Kriston Rickman
+%Date created 09/03/26
+%v1_00 for Energy based constraint analysis v1_19
+%States 
+%Notes: 
+
+
+
+classdef Aircraft_constraint_states
+    enumeration
+        MAIN_MENU
+        FIND_TW
+        GET_AIRCRAFT_DATA
+        GET_DESIRED_PERFORMANCE
+        CALCULATE_TW
+        DISPLAY_TW
+        ERROR_STATE
+    end
+end
