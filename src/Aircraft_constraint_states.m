5c00f185f81927b670874dbcd448aec46b0017a0
Kriston Rickman <kristonrickman@gmail.com>
2026-09-04 00:27:58 -0700

    v1_02 for v1_25
    We will not be using any type of event handle or notify in this program. I updated the list

--- src/Aircraft_constraint_states.m
+++ src/Aircraft_constraint_states.m
@@ -1,6 +1,6 @@
 %Created by Kriston Rickman
 %Date created 09/03/26
-%v1_01 for Energy based constraint analysis v1_21
+%v1_02 for Energy based constraint analysis v1_25
 %States 
 %Notes: 
 
@@ -9,6 +9,8 @@
 classdef Aircraft_constraint_states
     enumeration
         MAIN_MENU
+        SAVED_TW_AIRCRAFT_DATA
+        EDIT_SAVED_TW_AIRCRAFT
         GET_TW_AIRCRAFT_DATA
         CALCULATE_TW
         DISPLAY_TW
